08a1d46cd5b79bbe13df533148705a4dbdc482b5
JC Duran <liamlee.production@gmail.com>
2025-10-24 23:18:45 -0400

    add image and file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://liamleeproduction-cloud.github.io/grufos-page/",
+  site: "https://liamleeproduction-cloud.github.io",
   base: "/grufos-page/",
   // trailingSlash: "always"
 });
