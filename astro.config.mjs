9bc9305165db7da49a9ff8b79474853a36a71640
liamleeproduction-cloud <liamlee.production@gmail.com>
2025-10-25 01:56:40 +0000

    Update site URL in astro.config.mjs to reflect correct path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://liamleeproduction-cloud.github.io",
+  site: "https://liamleeproduction-cloud.github.io/grufos-page/",
   base: "/grufos-page/",
   // trailingSlash: "always"
 });
